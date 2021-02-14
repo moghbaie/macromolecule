c7c3bfa03d5c43a8cbc6d5935c304e223a51122a
Solange <47780566+solmarbio@users.noreply.github.com>
2021-02-14 22:58:10 +0100

    finishing assignment

--- mol-assignment.R
+++ mol-assignment.R
@@ -1,28 +1,28 @@
-#setting libraries
+### libraries needed
 library(readr)
 library(tidyverse)
 library(stringr)
 library(dplyr)
-library(reshape2)
 library(ggplot2)
 
-#importing the data
+### importing the data
 proteinGroups <- read_delim("https://raw.githubusercontent.com/moghbaie/L1_CRC_IP_MS/master/Input_data/Fourth_Run_04202019/txt/proteinGroups.txt", "\t", escape_double = FALSE, trim_ws = TRUE)
-names(proteinGroups)
+names(proteinGroups) 
 
-#filtering out the contaminants and reverse sequence, selecting the LFQ intensity columns
+### filtering out the contaminants and reverse sequence, selecting the LFQ intensity columns
 proteinGroups_clean <- proteinGroups %>%
   filter(!str_detect(`Majority protein IDs`, "CON") &
            !str_detect(`Majority protein IDs`, "REV")) %>%
   subset(select = c(1, 7, 233:262)) 
 
-#set 0 values to NA
+### log2 transformation of the LFQ intensity
+# first set 0 values to NA because we get -inf otherwise
 proteinGroups_clean[proteinGroups_clean == 0] <- NA
 
-#log transformation
 proteinGroups_clean[,3:32] <- log2(proteinGroups_clean[3:32])
 
-#calculating average per condition
+### calculating average log2 LFQ per condition
+# each condition has 3 replicates
 avgLFQ <- proteinGroups_clean[,3:32]
 n <- 1:ncol(avgLFQ)
 ind <- matrix(c(n, rep(NA, 3 - ncol(avgLFQ)%%3)), byrow=TRUE, ncol=3)
@@ -31,35 +31,59 @@ avgLFQ <- as.data.frame(do.call(cbind, lapply(ind, function(i) rowMeans(avgLFQ[,
 colnames(avgLFQ) <- c("c10", "c2", "c1", "c9", "c5", 
                       "c4", "c3", "c8", "c7", "c6")
 
-#calculating log2foldchange
-#foldch163T <- avgLFQ[,9:10] - avgLFQ[,8]
-#foldch159T <- avgLFQ[,6:7] - avgLFQ[,5]
-#foldch.all <- cbind(avgLFQ[,5], foldch159T, avgLFQ[,8], foldch163T)
-names(avgLFQ)
+## calc.log2foldchange 
+avgLFQ[sapply(avgLFQ, is.nan)] <- NA
 
-foldchresult <- as.data.frame(apply(avgLFQ,1, function(x){mean(x[c(1:4,6:7,9:10)]) - mean(x[c(5,8)])}))
+avgLFQ$tumor <- rowMeans(avgLFQ[,c(1:4,6:7,9:10)], na.rm = TRUE) #all tissues are pulled
+avgLFQ$ctrl <- rowMeans(avgLFQ[,c(5,8)], na.rm=TRUE)
+avgLFQ$ctrl[sapply(avgLFQ$ctrl, is.nan)] <- 0
 
-#t-test for all records
-ttestresult <- apply(foldch.all,1,function(x)try({t.test(x[c(1,4)],x[c(2,3,5,6)])$p.value}, silent = TRUE))
-padjustresult <- as.data.frame(p.adjust(ttestresult, method = "bonferroni"))
+avgLFQ$log2foldchange <- avgLFQ$tumor - avgLFQ$ctrl
 
-#consolidating
-LFQintensity_clean <- cbind(proteinGroups_clean[,1:2], foldchresult, padjustresult)
+### computing t-test for each sample
+# first setting data by different tissues
+foldch163T <- avgLFQ[,9:10] - avgLFQ$ctrl
+foldch159T <- avgLFQ[,6:7] - avgLFQ$ctrl
+foldch144T <- avgLFQ[,1:4] - avgLFQ$ctrl
+foldch.tumours <- cbind(foldch144T, foldch159T, foldch163T, avgLFQ[,5], avgLFQ[,8])
+
+# as there are not enough triplicates the t-test will fail for some samples
+foldch.tumours$pvalue <- apply(foldch.tumours,1,function(x)try({t.test(x[c(9,10)],x[c(1:8)])$p.value}, silent = TRUE))
+foldch.tumours$padjust <- p.adjust(foldch.tumours$pvalue, method = "bonferroni")
+
+### making the volcano plot 
+# first consolidating results into one clean table
+LFQintensity_clean <- cbind(proteinGroups_clean[,1:2], avgLFQ$log2foldchange, foldch.tumours$padjust)
 colnames(LFQintensity_clean) <- c("Protein.ID", "Gene.Name", "Log2FoldChange", "Adjusted.PValue")
 
-#plotting
-# add a column of NAs
+# establishing which proteins are differentially expressed
+# start with column of NAs
 LFQintensity_clean$diffexpressed <- "NO"
-# if log2Foldchange > 0.6 and pvalue < 0.05, set as "UP" 
+# if log2Foldchange > 0.6 and pvalue < 0.05, "UP" 
 LFQintensity_clean$diffexpressed[LFQintensity_clean$Log2FoldChange > 0.6 & LFQintensity_clean$Adjusted.PValue < 0.05] <- "UP"
-# if log2Foldchange < -0.6 and pvalue < 0.05, set as "DOWN"
+# if log2Foldchange < -0.6 and pvalue < 0.05, "DOWN"
 LFQintensity_clean$diffexpressed[LFQintensity_clean$Log2FoldChange < -0.6 & LFQintensity_clean$Adjusted.PValue < 0.05] <- "DOWN"
 
-diffplot <- ggplot(data=LFQintensity_clean, aes(x=Log2FoldChange, y=-log10(Adjusted.PValue), col=diffexpressed, label = Gene.Name)) + 
-  geom_point() + theme_minimal() + 
+diffplot <- ggplot(data=LFQintensity_clean, aes(x=Log2FoldChange, y=-log10(Adjusted.PValue), col=diffexpressed)) + 
+  geom_point() + theme_minimal() +
   geom_vline(xintercept=c(-0.6, 0.6), col="red") +
   geom_hline(yintercept=-log10(0.05), col="red") +
   scale_color_manual(values=c("blue", "black", "red")) +
-  theme(legend.position = "none")
+  theme(legend.position = "none") +
+  xlim(-8, 12) + ylim(0, 10)
 diffplot
 
+# if you want to see the name of the differentially expressed proteins we can add this:
+LFQintensity_clean$label <- NA
+LFQintensity_clean$label[LFQintensity_clean$diffexpressed != "NO"] <- LFQintensity_clean$Gene.Name[LFQintensity_clean$diffexpressed != "NO"]
+
+# plot version 2
+library(ggrepel)
+diffplot_2 <- ggplot(data=LFQintensity_clean, aes(x=Log2FoldChange, y=-log10(Adjusted.PValue), col=diffexpressed, label = label)) + 
+  geom_point() + theme_minimal() + geom_text() + geom_text_repel() +
+  geom_vline(xintercept=c(-0.6, 0.6), col="red") +
+  geom_hline(yintercept=-log10(0.05), col="red") +
+  scale_color_manual(values=c("blue", "black", "red")) +
+  theme(legend.position = "none") +
+  xlim(-8, 12) + ylim(0, 10)
+diffplot_2
